9570e790ba41bbfa3f0bba5ac0559ef850ddf6e6
student <student@kr.step>
2023-05-10 20:32:34 +0300

    new_file

---
+++ pr/PR5_modul_04_function_c1/PR1.py
@@ -0,0 +1,8 @@
+# Напишите функцию, которая отображает на экран форматированный текст, указанный ниже:
+
+def print_text():
+    print("Don't let the noise of others' opinions")
+    print("drown out your own inner voice.")
+    print("Steve Jobs")
+
+print_text()
