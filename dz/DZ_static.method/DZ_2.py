be143e50662c4564b55c42d87aa184c59b695ae7
cdtnkfyf1234 <127993438+cdtnkfyf1234@users.noreply.github.com>
2023-08-01 17:52:18 +0300

    DZ_overdue

--- dz/DZ_static.method/DZ_2.py
+++ dz/DZ_static.method/DZ_2.py
@@ -25,3 +25,16 @@ print(fahrenheit)
 c = int (input ("Введіть с: ") )
 f = c * 9/5 + 32
 print (f)
+
+
+def convert(x):
+        tf=(9/5)*x+32
+        return tf
+while True:
+ tc=input('t v celciax ')
+ if tc=='':
+     break #или exit()
+ else :
+     tc=int(tc)
+ tf=convert(tc)
+ print('t в цельсиях',tc,'т в фаренгейтах',tf)
